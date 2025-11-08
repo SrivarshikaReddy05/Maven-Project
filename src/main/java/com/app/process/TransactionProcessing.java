2cbcd86cec94cf45a9c25c7b46eeefa0655429e4
srivarshikareddy@gmail.com <srivarshikareddy@gmail.com>
2025-11-08 10:28:35 +0530

    first commit

---
+++ src/main/java/com/app/process/TransactionProcessing.java
@@ -0,0 +1,15 @@
+package com.app.process;
+
+public class TransactionProcessing {
+	
+	public TransactionProcessing() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a Maven Project");
+
+	}
+
+}
